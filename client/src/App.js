5ff9b975e2cd4cb82aa401e265a2c323175851c8
Ernesto Neto <ernestoborges1995@gmail.com>
2022-12-18 19:02:03 -0300

    Added functional live chat, without any css or customization

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+import socketIO from 'socket.io-client';
+const socket = socketIO.connect('http://localhost:3333');
 
 function App() {
+  const [message, setMessage] = useState("")
+  const [messages, setMessages] = useState([])
+
+
+  const handdleSendMessage = (e) => {
+    e.preventDefault();
+    console.log({userName: "Me", message})
+    socket.emit("message", {
+      text: message,
+      name: "User",
+      id: `${socket.id}${Math.random()}`,
+      socketID: socket.id
+    })
+    setMessage("")
+  }
+
+  useEffect(() => {
+    socket.on("messageResponse", data => setMessages([...messages, data]))
+    console.log(messages)
+  }, [socket, messages])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <section className="live-chat-section">
+          <article>
+            <header>Live Chat</header>
+            <section className="chat-body">
+              {messages.map(chat => 
+                <p>{chat.text}</p>
+              )}
+            </section>
+            <form className="chat-type-bar" onSubmit={handdleSendMessage}>
+              <input
+                type="text"
+                placeholder="Write message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
+              <button>Send</button>
+            </form>
+          </article>
+        </section>
+      </main>
     </div>
   );
 }
--- server/app.js
+++ server/app.js
@@ -1,11 +1,38 @@
 const express = require("express")
 const app = express()
 
+const http = require('http').Server(app);
+const cors = require("cors")
+
+app.use(cors())
+
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
+socketIO.on('connection', (socket) => {
+    console.log(`${socket.id} user just connected!`);
+
+    // listen and logs the message to the console
+
+    socket.on('message', (data) => {
+        console.log(data)
+        socketIO.emit('messageResponse', data);
+    });
+    
+    socket.on('disconnect', () => {
+      console.log('A user disconnected');
+    });
+
+});
+
 app.get("/", (req, res)=> {
     res.send("Hello, world!")
 })
 
-const PORT = 3000
-app.listen(PORT, ()=>{
+const PORT = 3333
+http.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`)
 })
